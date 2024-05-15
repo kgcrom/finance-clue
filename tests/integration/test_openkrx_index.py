a9640c66a65da9ff3be9ec239c2a61ce47663cdf
kgcrom <kgcrom@hotmail.com>
2024-05-15 11:42:50 +0900

    feat: 채권, 파생상품 지수 oas 추가

--- tests/integration/test_openkrx_index.py
+++ tests/integration/test_openkrx_index.py
@@ -18,8 +18,26 @@ def test_kospi_daily_index_quotation(integration_openkrx_client: OpenKrxClient):
 
 
 def test_kosdaq_daily_index_quotation(integration_openkrx_client: OpenKrxClient):
-    resp = integration_openkrx_client.get_kosdaq_daily_index_quotation(bas_dd="20240513")
+    resp = integration_openkrx_client.get_kosdaq_daily_index_quotation(
+        bas_dd="20240513"
+    )
 
     assert resp is not None
     assert len(resp["OutBlock_1"]) == 52
     assert resp["OutBlock_1"][0]["IDX_CLSS"] == "KOSDAQ"
+
+
+def test_bond_daily_index_quotation(integration_openkrx_client: OpenKrxClient):
+    resp = integration_openkrx_client.get_bond_daily_index_quotation(bas_dd="20240513")
+
+    assert resp is not None
+
+
+def test_derivation_daily_index_quotation(integration_openkrx_client: OpenKrxClient):
+    resp = integration_openkrx_client.get_derivatives_daily_index_quotation(
+        bas_dd="20240513"
+    )
+
+    assert resp is not None
+    assert len(resp["OutBlock_1"]) == 222
+    assert resp["OutBlock_1"][0]["IDX_CLSS"] == "선물지수"
--- tests/mocked/test_openkrx_index.py
+++ tests/mocked/test_openkrx_index.py
@@ -7,7 +7,7 @@ from finance_clue.openkrx import OpenKrxClient
 
 @responses.activate
 def test_krx_daily_index_quotation(
-        mock_openkrx_client: OpenKrxClient, mock_openkrx_client_url: str
+    mock_openkrx_client: OpenKrxClient, mock_openkrx_client_url: str
 ):
     expected = _generate_expected("KRX")
 
@@ -27,9 +27,9 @@ def test_krx_daily_index_quotation(
 
 @responses.activate
 def test_kospi_daily_index_quotation(
-        mock_openkrx_client: OpenKrxClient, mock_openkrx_client_url: str
+    mock_openkrx_client: OpenKrxClient, mock_openkrx_client_url: str
 ):
-    expected = _generate_expected("KRX")
+    expected = _generate_expected("KOSPI")
 
     responses.add(
         responses.GET,
@@ -47,9 +47,9 @@ def test_kospi_daily_index_quotation(
 
 @responses.activate
 def test_kosdaq_daily_index_quotation(
-        mock_openkrx_client: OpenKrxClient, mock_openkrx_client_url: str
+    mock_openkrx_client: OpenKrxClient, mock_openkrx_client_url: str
 ):
-    expected = _generate_expected("KRX")
+    expected = _generate_expected("KOSDAQ")
 
     responses.add(
         responses.GET,
@@ -59,6 +59,80 @@ def test_kosdaq_daily_index_quotation(
     )
 
     resp = mock_openkrx_client.get_kosdaq_daily_index_quotation(
+        bas_dd="20240514",
+    )
+
+    assert resp == expected
+
+
+@responses.activate
+def test_bond_daily_index_quotation(
+    mock_openkrx_client: OpenKrxClient, mock_openkrx_client_url: str
+):
+    expected = {
+        "OutBlock_1": [
+            {
+                "BAS_DD": "20200414",
+                "BND_IDX_GRP_NM": "KRX 채권지수",
+                "TOT_EARNG_IDX": "189.02",
+                "TOT_EARNG_IDX_CMPPREVDD": "0.03",
+                "NETPRC_IDX": "110.03",
+                "NETPRC_IDX_CMPPREVDD": "0.01",
+                "ZERO_REINVST_IDX": "183.02",
+                "ZERO_REINVST_IDX_CMPPREVDD": "0.03",
+                "CALL_REINVST_IDX": "186.33",
+                "CALL_REINVST_IDX_CMPPREVDD": "0.03",
+                "MKT_PRC_IDX": "111.86",
+                "MKT_PRC_IDX_CMPPREVDD": "0.02",
+                "AVG_DURATION": "5.670",
+                "AVG_CONVEXITY_PRC": "87.205",
+                "BND_IDX_AVG_YD": "1.338",
+            }
+        ]
+    }
+
+    responses.add(
+        responses.GET,
+        f"{mock_openkrx_client_url}/svc/apis/idx/bon_dd_trd",
+        json=expected,
+        status=200,
+    )
+
+    resp = mock_openkrx_client.get_bond_daily_index_quotation(
+        bas_dd="20240514",
+    )
+
+    assert resp == expected
+
+
+@responses.activate
+def test_derivative_daily_index_quotation(
+    mock_openkrx_client: OpenKrxClient, mock_openkrx_client_url: str
+):
+    expected = {
+        "OutBlock_1": [
+            {
+                "BAS_DD": "20200414",
+                "IDX_CLSS": "선물지수",
+                "IDX_NM": "코스피 200 선물지수",
+                "CLSPRC_IDX": "1212.67",
+                "CMPPREVDD_IDX": "23.59",
+                "FLUC_RT": "1.98",
+                "OPNPRC_IDX": "1202.10",
+                "HGPRC_IDX": "1219.55",
+                "LWPRC_IDX": "1195.96",
+            }
+        ]
+    }
+
+    responses.add(
+        responses.GET,
+        f"{mock_openkrx_client_url}/svc/apis/idx/drvprod_dd_trd",
+        json=expected,
+        status=200,
+    )
+
+    resp = mock_openkrx_client.get_derivatives_daily_index_quotation(
         bas_dd="20240514",
     )
 
